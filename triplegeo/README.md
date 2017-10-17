a44525dc262a63945020ab02790f7372af5510e7
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2017-10-17 22:05:54 +0300

    Review entrypoint script (environment overrides for configuration file)

--- triplegeo/README.md
+++ triplegeo/README.md
@@ -8,6 +8,8 @@ Build image:
 
 ### Examples
 
+#### Example: Reading from a SHAPEFILE (SHP)
+
 Transform a SHAPEFILE `points.shp` (with accompanying `prj`, `dbf`, `shx` files) to triplets. Run container 
 by bind-mounting the proper input/output volumes:
 
@@ -18,5 +20,38 @@ by bind-mounting the proper input/output volumes:
         -env INPUT_FILE=/var/local/triplegeo/input/points.shp \
         --volume "$(pwd)/samples/shapefile/1/output:/var/local/triplegeo/output:rw" \
         --tmpfs /tmp:size=128M 
+        local/triplegeo:1.2
+
+#### Example: Reading from a plain CSV file
+
+Transform a group of CSV files (given as a colon-separated string) `osm_pois_part*.csv` to triplets. Each input
+file generates a corresponding output file (e.g for `TTL` serialization, that would be `osm_pois_part*.ttl`).
+    
+    docker run -t --rm \
+        --name triplegeo-1 \
+        --volume "$(pwd)/samples/csv/1/options.conf:/var/local/triplegeo/options.conf:ro" \
+        --volume "$(pwd)/samples/csv/1/input:/var/local/triplegeo/input:ro" \
+        -env INPUT_FILE=/var/local/triplegeo/input/osm_pois_part1.csv:/var/local/triplegeo/input/osm_pois_part2.csv:/var/local/triplegeo/input/osm_pois_part3.csv \
+        --volume "$(pwd)/samples/csv/1/output:/var/local/triplegeo/output:rw" \
+        local/triplegeo:1.2
+
+#### Example: Reading from a database table
+
+Transform columns of a database table to triplets. A sample table SQL dump (suitable for testing this configuration) can be found 
+under `samples/jdbc/1/towns.sql.gz`.
+
+Now, there is no `INPUT_FILE` environment variable, instead we provide all table-related configuration inside our (externally provided)
+configuration file. The connection-related parameters can all be overriden by environment variables passed at runtime. Note that if a 
+password is needed to connect to the database, the password file must be bind-mounted into the container.
+
+    docker run -t --rm \
+        --name triplegeo-1 \
+        --volume "$(pwd)/samples/jdbc/1/options.conf:/var/local/triplegeo/options.conf:ro" \
+        --volume "$(pwd)/samples/jdbc/1/output:/var/local/triplegeo/output:rw" \
+        --link postgres-1:db-server
+        --env DB_URL=jdbc:postgresql://db-server:5432/triplegeo-examples \
+        --env DB_USERNAME=slipo \
+        --volume "$(pwd)/secrets/password:/var/local/triplegeo/secrets/password:ro" \
+        --env DB_PASSWORD_FILE=/var/local/triplegeo/secrets/password \
         local/triplegeo:1.2
 
--- triplegeo/entrypoint.sh
+++ triplegeo/entrypoint.sh
@@ -2,33 +2,86 @@
 
 set -e
 
-CLASSPATH=target/triplegeo-${TRIPLEGEO_VERSION}.jar
+CLASSPATH="target/triplegeo-${TRIPLEGEO_VERSION}.jar:target/lib/*"
 
+MAIN_CLASS=eu.slipo.athenarc.triplegeo.Executor
 
 if [ -z "${CONFIG_FILE}" ]; then
-    echo "Cannot find configuration file (CONFIG_FILE)!" 1>&2
+    echo "Cannot find configuration file (specify CONFIG_FILE in environment)!" 1>&2
     exit 1
 fi
 
 #
-# Build a custom configuration file (derived from given configuration) 
+# Build a custom configuration file based on environment variables and given configuration file
 #
 
 config_file=$(mktemp -p /var/local/triplegeo -t options-XXXXXXX.conf)
 cp ${CONFIG_FILE} ${config_file}
 
-if [ -f "${INPUT_FILE}" ]; then
-    sed -i -e "s~^inputFiles[ ]*=[ ]*.*$~inputFiles = ${INPUT_FILE}~"  ${config_file}
-fi
-
 if [ -f "${OUTPUT_DIR}" ]; then
     sed -i -e "s~^outputDir[ ]*=[ ]*.*$~outputDir = ${OUTPUT_DIR}~"  ${config_file}
 fi
+
+# The INPUT_FILE may be a single file or a list of colon-separated input files
+if [ -n "${INPUT_FILE}" ]; then
+    # Split and join by ';' as expected by triplegeo
+    input_files=$(IFS=':' p=( $INPUT_FILE ); IFS=';'; echo "${p[*]}")
+    # Edit configuration file
+    sed -i -e "s~^inputFiles[ ]*=[ ]*.*$~inputFiles = ${input_files}~"  ${config_file}
+fi
+
+if [ -n "${DB_URL}" ]; then
+    if [ -n "${INPUT_FILE}" ]; then
+        echo "The input resources are contradicting (DB_URL and INPUT_FILE are both present)!" 1>&2;
+        exit 1;
+    fi
+    # Parse a url expected as "jdbc:<jdbc-provider>://<host>:<port>/<database>". 
+    if [[ ! "${DB_URL}" =~  ^jdbc: ]]; then
+        echo "The connection URL is malformed" 1>&2
+        echo "Note: The connection URL is expected as jdbc:<jdbc-provider>://<host>[:<port>]/<database>" 1>&2;
+        exit 1;
+    fi
+    url=${DB_URL#jdbc:}
+    db_provider=$(IFS=: p=( $url ); echo ${p[0]})
+    if [ -z "${db_provider}" ]; then
+        echo "The connection URL is malformed (no database provider?)" 1>&2;
+        exit 1;
+    else 
+        # Parse connection url
+        u1=${url#${db_provider}://}
+        db_address=$(IFS='/' p=( $u1 ); echo ${p[0]})
+        db_name=$(IFS='/' p=( $u1 ); echo ${p[1]})
+        db_host=$(IFS=':' p=( $db_address ); echo ${p[0]})
+        db_port=$(IFS=':' p=( $db_address ); echo ${p[1]})
+        # Map to triplegeo-specific dbType, add port defaults
+        if [[ ${db_provider} == 'postgresql' ]]; then
+            db_type='PostGIS'
+            db_port=${db_port:-"5432"}
+        elif [[ ${db_provider} == 'mysql' ]]; then
+            db_type='MySQL'
+            db_port=${db_port:-"3306"}
+        fi
+        # Edit configuration file
+        sed -i -e "s~^dbType[ ]*=[ ]*.*$~dbType = ${db_type}~"  ${config_file}
+        sed -i -e "s~^dbName[ ]*=[ ]*.*$~dbName = ${db_name}~"  ${config_file}
+        sed -i -e "s~^dbHost[ ]*=[ ]*.*$~dbHost = ${db_host}~"  ${config_file}
+        sed -i -e "s~^dbPort[ ]*=[ ]*.*$~dbPort = ${db_port}~"  ${config_file}
+    fi
+fi
+
+if [ -n "${DB_USERNAME}" ]; then
+    sed -i -e "s~^dbUserName[ ]*=[ ]*.*$~dbUserName = ${DB_USERNAME}~"  ${config_file}
+fi
+
+if [ -n "${DB_PASSWORD_FILE}" ] && [ -f "${DB_PASSWORD_FILE}" ]; then
+    db_password=$(cat ${DB_PASSWORD_FILE})
+    sed -i -e "s~^dbPassword[ ]*=[ ]*.*$~dbPassword = ${db_password}~"  ${config_file}
+fi
+
 
 #
 # Run command
 #
 
-MAIN_CLASS=eu.slipo.athenarc.triplegeo.Executor
-
 exec java ${JAVA_XX_OPTS} ${JAVA_MEM_OPTS} -cp ${CLASSPATH} ${MAIN_CLASS} ${config_file}
+
