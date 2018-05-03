e76d5eda9ae9d36ce30693384211c577ca102655
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2018-05-03 21:27:30 +0300

    Update examples for Fagi

---
+++ fagi/run-with-sample.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sample_number=${1}
+test -z "${sample_number}" && sample_number=1
+
+if [ ! -d "samples/${sample_number}" ]; then
+    echo "No such directory (samples/$sample_number)" && exit 1
+fi
+
+docker run -it --rm --name fagi-1 \
+    --volume "$(pwd)/samples/${sample_number}/rules.xml:/var/local/fagi/rules.xml:ro" \
+    --volume "$(pwd)/samples/${sample_number}/input/a.nt:/var/local/fagi/input/a.nt:ro" \
+    --volume "$(pwd)/samples/${sample_number}/input/b.nt:/var/local/fagi/input/b.nt:ro" \
+    --volume "$(pwd)/samples/${sample_number}/input/links.nt:/var/local/fagi/input/links.nt:ro" \
+    --volume "$(pwd)/volumes/${sample_number}/output:/var/local/fagi/output" \
+    --env TARGET_MODE=AA_MODE \
+    --env TARGET_FUSED_NAME=fused --env TARGET_REMAINING_NAME=remaining --env TARGET_REVIEW_NAME=review \
+    athenarc/fagi:1.2
+
