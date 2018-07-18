20243c317884f2d68c849a8bbca2d42aef4c44c8
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2018-07-18 13:41:33 +0300

    Update Fagi testing samples

--- fagi/run-with-sample.sh
+++ fagi/run-with-sample.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-sample_number=${1}
-test -z "${sample_number}" && sample_number=1
+sample=${1}
 
-if [ ! -d "samples/${sample_number}" ]; then
-    echo "No such directory (samples/$sample_number)" && exit 1
+test -z "${sample}" && sample=1
+
+spec_properties_file="samples/${sample}/spec.properties"
+
+if [ ! -d "samples/${sample}" ]; then
+    echo "No such directory (samples/$sample)" && exit 1;
+fi
+if [ ! -f "${spec_properties_file}" ]; then
+    echo "This sample contains no configuration!" && exit 1;
 fi
 
-docker run -it --rm --name fagi-t${sample_number} \
-    --volume "$(pwd)/samples/${sample_number}/rules.xml:/var/local/fagi/rules.xml:ro" \
-    --volume "$(pwd)/samples/${sample_number}/input/a.nt:/var/local/fagi/input/a.nt:ro" \
-    --volume "$(pwd)/samples/${sample_number}/input/b.nt:/var/local/fagi/input/b.nt:ro" \
-    --volume "$(pwd)/samples/${sample_number}/input/links.nt:/var/local/fagi/input/links.nt:ro" \
-    --volume "$(pwd)/volumes/${sample_number}/output:/var/local/fagi/output" \
-    --env TARGET_MODE=A_MODE \
+fusion_mode=$(cat "${spec_properties_file}" | grep -o -P -e '(?<=^target\.mode[[:space:]]=[[:space:]])([^[:space:]])+$')
+
+container_name="fagi-test-$(echo -n "${sample}"| tr '[:space:]-' '_' | tr '[:upper:]' '[:lower:]')"
+docker run -it --name ${container_name} \
+    --volume "$(pwd)/samples/${sample}/rules.xml:/var/local/fagi/rules.xml:ro" \
+    --volume "$(pwd)/samples/${sample}/input/a.nt:/var/local/fagi/input/a.nt:ro" \
+    --volume "$(pwd)/samples/${sample}/input/b.nt:/var/local/fagi/input/b.nt:ro" \
+    --volume "$(pwd)/samples/${sample}/input/links.nt:/var/local/fagi/input/links.nt:ro" \
+    --volume "$(pwd)/volumes/${sample}/output:/var/local/fagi/output" \
+    --env TARGET_MODE=${fusion_mode} \
     --env TARGET_FUSED_NAME=fused \
     --env TARGET_REMAINING_NAME=remaining \
     --env TARGET_REVIEW_NAME=review \
